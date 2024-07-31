c5de44c4b04e569078f0ca6e20793d23586fd3ad
Dmitriy <homyakov-dmitriy@mail.ru>
2024-07-31 11:20:39 +0200

    Merge nums1 and nums2 into a single array(nums1) in non-decreasing order

---
+++ MergeSortedArray/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace MergeSortedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int[] nums2 = { 2, 5, 6 };
+            int m = 3;
+            int n = 3;
+
+            solution.Merge(nums1, m, nums2, n);
+
+            foreach (int i in nums1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    public class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i = m - 1;
+            int j = n - 1;
+            int k = m + n - 1;
+
+            while (j >= 0)
+            {
+                if (i >= 0 && nums1[i] > nums2[j])
+                {
+                    nums1[k] = nums1[i];
+                    i--;
+                }
+                else
+                {
+                    nums1[k] = nums2[j];
+                    j--;
+                }
+                k--;
+            }
+        }
+    }
+}
