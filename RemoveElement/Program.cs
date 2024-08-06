8bfd5f89a2c29ee5c251ad4d6f02bb9ca8138e99
Dmitriy <homyakov-dmitriy@mail.ru>
2024-08-06 17:22:34 +0200

    Changing the array such that the first k elements contain the elements which are not equal to val

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace RemoveElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = { 3, 2, 2, 3 };
+            int val = 3;
+            Solution magority = new Solution();
+            int k = magority.RemoveElement(nums, val);
+
+            Console.WriteLine(k);
+        }
+    }
+
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == val || nums[i] == -1)
+                {
+                    for (int j = i + 1; j < nums.Length; j++)
+                    {
+                        if (nums[j] != val && nums[j] != -1)
+                        {
+                            nums[i] = nums[j];
+                            nums[j] = -1;
+                            k++;
+                            break;
+                        }
+                    }
+                }
+                else
+                    k++;
+            }
+            return k;
+        }
+    }
+}
