533a4f2c2eb57e9bb50b9ed0fa661659f9eb0a4b
Dmitriy <homyakov-dmitriy@mail.ru>
2024-08-06 17:28:58 +0200

    Removing the duplicates in-place such that each unique element appears only once

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace RemoveDuplicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+            Solution solution = new Solution();
+            int k = solution.RemoveDuplicates(nums);
+
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine(k);
+        }
+    }
+
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int length = nums.Length;
+            if (length == 0) return 0;
+            int checkNum = nums[0];
+            int currentIndex = 1;
+            int k = 1;
+            int i = 1;
+            while (true)
+            {
+                if (i == length) break;
+                if (nums[i] <= checkNum)
+                {
+                    i++;
+                }
+                else
+                {
+                    nums[currentIndex++] = nums[i];
+                    checkNum = nums[i];
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+}
