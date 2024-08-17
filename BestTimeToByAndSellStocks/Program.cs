8f2ca6c11d9a162c302ea101caf311eb48bc7a13
Dmitriy <homyakov-dmitriy@mail.ru>
2024-08-17 17:11:08 +0200

    Returning the maximum profit that can be achieved from this transaction.

---
+++ BestTimeToByAndSellStocks/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Numerics;
+
+namespace BestTimeToByAndSellStocks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 7, 1, 5, 3, 6, 4 };
+
+            Solution solution = new Solution();
+            int result = solution.MaxProfit(nums);
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int profit = 0;
+            int min = prices[0];
+            int max = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] >= min && prices[i] <= max)
+                    continue;
+
+                if (prices[i] < min)
+                {
+                    min = prices[i];
+                    max = min;
+                    continue;
+                }
+
+                if (prices[i] > max)
+                {
+                    max = prices[i];
+                    profit = max - min > profit ? max - min : profit;
+                }
+            }
+            return profit;
+        }
+    }
+}
